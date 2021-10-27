c1208df90758dcf8a9d838a0a743594a6424df03
marorcua <ortiz.mario22@gmail.com>
2021-10-27 10:11:40 +0200

    done useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { User } from './interfaces'
 
 interface AppProps {
-  header: string
+  header: string,
+  extraText?: string,
 }
 
-function App({ header }: AppProps) {
+function App({ header, extraText = 'default text' }: AppProps) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const fetchUser = () => setUser({
+    name: 'Michael',
+    age: 23,
+    country: 'USA',
+    address: {
+      street: 'average street',
+      number: 5,
+      zip: 23030
+    },
+    admin: false
+  })
+
   return (
-    <h1>{header}</h1>
+    <>
+      <h1>{header}</h1>
+      {extraText && <p>{extraText}</p>}
+      <button onClick={fetchUser}>Fetch user</button>
+      {user &&
+        <p>{`Welcome user ${user.name}`}</p>}
+    </>
   );
 }
 
